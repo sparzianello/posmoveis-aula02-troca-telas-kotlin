438fa09d6c75e92e079503b769b218f13f80c14a
Samuel Parzianello <parzianellosamuel2@gmail.com>
2025-03-22 07:53:56 -0300

    upload git
    versão inicial da aplicação de navegação e transferência de dados entre telas da aula 02 da pós graduação de dispositivos móveis da UTFPR 2025.

---
+++ app/src/main/java/posmoveis/sparzianello/troca_telas/FormLancamentoActivity.kt
@@ -0,0 +1,11 @@
+package posmoveis.sparzianello.troca_telas
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FormLancamentoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_lancamento)
+    }
+}
---
+++ app/src/main/java/posmoveis/sparzianello/troca_telas/MainActivity.kt
@@ -0,0 +1,21 @@
+package posmoveis.sparzianello.troca_telas
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun btnNovoRegistroOnClick(view: View) {
+        val intent = Intent(this, FormLancamentoActivity::class.java)
+        startActivity(intent)
+    }
+}
